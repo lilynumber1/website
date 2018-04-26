94b76ae9f422ce220a5b82a7f4d1faaacd1bbadc
lilyguan <106589142@163.com>
2018-04-26 18:36:11 +0800

    添加兑换码功能

---
+++ welfare/cdk.php
@@ -0,0 +1,117 @@
+<?php
+include("smarty_inc.php");
+
+if($_GET["cdk"]){
+    //验证兑换码
+    $is_right = verifyCode($_GET["cdk"]);
+
+    if($is_right){
+        header("Location: https://m.51nafuli.com/index.jsp");
+    }else{
+        $smarty->assign("placeTxt", "错误的兑换码！");
+        $smarty->display("registerByCDK.html");
+    }
+}else{
+    echo '<h1>Promotion / Discount Codes</h1>';
+    echo '<pre>';
+    print_r(generate_promotion_code(10000, 5));
+    echo '</pre>';
+}
+
+function generate_promotion_code($no_of_codes, $code_length = 4)
+{
+    $characters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    $promotion_codes = "";//用来接收生成的优惠码
+    for ($j = 0; $j < $no_of_codes; $j++) {
+        $code = "";
+        for ($i = 0; $i < $code_length; $i++) {
+            $code .= $characters[mt_rand(0, strlen($characters) - 1)];
+        }
+        //如果生成的4位随机数不在我们定义的$promotion_codes函数里面
+        if($promotion_codes == ""){
+            $promotion_codes .= "('" . $code . "'),";//将优惠码赋值给数组
+        }elseif (strpos($promotion_codes,$code) === FALSE){
+            if($j == $no_of_codes - 1){
+                $promotion_codes .= "('" . $code . "')";//将优惠码赋值给数组
+            }else{
+                $promotion_codes .= "('" . $code . "'),";//将优惠码赋值给数组
+            }
+        }else {
+            $j--;
+        }
+    }
+    createCodes($promotion_codes);
+    return $promotion_codes;
+}
+
+function createCodes($cdks){
+    $servername = "qdm11426228.my3w.com";
+    $username = "qdm11426228";
+    $password = "lily20150610";
+    $dbname = "qdm11426228_db";
+
+// 创建连接
+    $conn = new mysqli($servername, $username, $password, $dbname);
+// 检测连接
+    if ($conn->connect_error) {
+        die("连接失败: " . $conn->connect_error);
+    }
+
+    $log_str = "";
+    $tablename = "tb_cdks";
+
+// 使用 sql 创建数据表
+    $sql = "CREATE TABLE IF NOT EXISTS " . $tablename . " (cdkcode VARCHAR(10) PRIMARY KEY)";
+
+    if ($conn->query($sql) === TRUE) {
+        $log_str = $log_str . " Table " . $tablename . " created successfully ";
+
+        $sql = "INSERT IGNORE INTO " . $tablename . " (cdkcode) VALUES $cdks";
+        if ($conn->query($sql) === TRUE) {
+            $log_str = $log_str . " 新记录插入成功 ";
+        } else {
+            $log_str = $log_str . " Error: " . $sql . "<br>" . $conn->error;
+        }
+    } else {
+        $log_str = $log_str . "创建数据表错误: " . $conn->error;
+    }
+
+    echo "console.log('".$log_str."')";
+    $conn->close();
+}
+
+function verifyCode($cdk){
+    $servername = "qdm11426228.my3w.com";
+    $username = "qdm11426228";
+    $password = "lily20150610";
+    $dbname = "qdm11426228_db";
+
+// 创建连接
+    $conn = new mysqli($servername, $username, $password, $dbname);
+// 检测连接
+    if ($conn->connect_error) {
+        die("连接失败: " . $conn->connect_error);
+    }
+
+    $log_str = "";
+    $tablename = "tb_cdks";
+
+// 使用 sql 创建数据表
+    $sql = "CREATE TABLE IF NOT EXISTS " . $tablename . " (cdkcode VARCHAR(10) PRIMARY KEY)";
+    $is_right = false;
+    if ($conn->query($sql) === TRUE) {
+        $log_str = $log_str . " Table " . $tablename . " created successfully ";
+
+        $sql = "SELECT * FROM " . $tablename . " WHERE cdkcode='".$cdk."'";
+        $result = $conn->query($sql);
+        if ($result->num_rows > 0) {
+            $is_right = true;
+        }
+    } else {
+        $log_str = $log_str . "创建数据表错误: " . $conn->error;
+    }
+//    echo "<script>console.log('".$log_str."');</script>";
+    $conn->close();
+    return $is_right;
+}
+?>
--- welfare/index.php
+++ welfare/index.php
@@ -1,64 +1,66 @@
 <?php
 include("smarty_inc.php");
 
-$smarty->assign("link4", "http://ah.189.cn/iphone/ext/lmwkfanliwap/index.html?DevCode=AH00F90005/Y34000099437/001");
-$smarty->assign("img4", "./img/4.jpg");
-$smarty->assign("title4", "前2个月免费使用");
-$smarty->assign("title41", "全国可申请，开卡次月返10元");
-$smarty->assign("desc41", "1、该卡可以全国申请，不限安徽。");
-$smarty->assign("desc42", "2、月租19元，可畅享6G全国流量。");
-$smarty->assign("desc43", "3、前2个月免费使用。");
-$smarty->assign("id4","liuliang19");
-
-$smarty->assign("link5", "http://ah.189.cn/wap/combo/CATEGORY_combo4G_49gnbxl.htm?DevCode=AH00F90005/Y34000099437/001");
-$smarty->assign("img5", "./img/5.jpg");
-$smarty->assign("title5", "全国流量不限量仅需49元/月");
-$smarty->assign("title51", "全国可申请，开卡次月返30元");
-$smarty->assign("desc51", "1、该卡可以全国申请，不限安徽。");
-$smarty->assign("desc52", "2、月租49元，国内流量不限量使用。");
-$smarty->assign("desc53", "3、开卡次月返30元，以话费形式冲到开卡号码账户中。");
-$smarty->assign("id5","liuliang49");
-
-$smarty->assign("link6", "http://game.fire2333.com/home/ac?action=/home/game/a/1809/g/200002/pt/8397");
-$smarty->assign("img6", "./img/6.jpg");
-$smarty->assign("title6", "抢夺王城霸主");
-$smarty->assign("title61", "热血开战！");
-$smarty->assign("id6","youxi");
-
-$smarty->assign("link7", "http://m.ycsbar.com/link/52037691");
-$smarty->assign("img7", "./img/7.jpg");
-$smarty->assign("title7", "闪婚闪离的95后女孩");
-$smarty->assign("title71", "我结婚是为了合法约P!");
-$smarty->assign("id7","taohuaxiaocunyi");
-
-$smarty->assign("link8", "http://m.ycsbar.com/link/71423608");
-$smarty->assign("img8", "./img/8.jpg");
-$smarty->assign("title8", "一夜成欢 ");
-$smarty->assign("title81", "一气之下我跟小白脸滚了床单··· ");
-$smarty->assign("id8","yiyechenghuan");
-
-$smarty->assign("link9", "https://c.pingan.com/apply/mobile/apply/index.html?scc=950000773&ccp=1a2a3a4a5a8a9a10a11a12a13&showt=0");
-$smarty->assign("img9", "./img/9.jpg");
-$smarty->assign("title9", "超高额度快速发卡");
-$smarty->assign("title91", "发卡成功奖励30元话费");
-$smarty->assign("id9","pinganxinyongka");
-
-$smarty->assign("link10", "http://dwz.cn/7DQDl3");
-$smarty->assign("img10", "./img/10.jpg");
-$smarty->assign("title10", "超高额度快速发卡");
-$smarty->assign("title101", "发卡成功奖励20元话费");
-$smarty->assign("id10","minshengxinyongka");
-
 if($_GET["action"]){
-//    $smarty->display("log.html");
     if($_GET["action"] == "buySucc"){
         $qudao = $_GET["param"] . "";
         $url = $_GET["param2"] . "";
         update_data($qudao, $url);
+    }else if($_GET["action"] == "generate"){
+        $smarty->display("generateCDK.html");
+    }else if($_GET["action"] == "verify"){
+        $smarty->assign("placeTxt", "兑换码");
+        $smarty->display("registerByCDK.html");
     }
-
-//    echo "callbackFunction('logsucc')";
 }else{
+    $smarty->assign("link4", "http://ah.189.cn/iphone/ext/lmwkfanliwap/index.html?DevCode=AH00F90005/Y34000099437/001");
+    $smarty->assign("img4", "./img/4.jpg");
+    $smarty->assign("title4", "前2个月免费使用");
+    $smarty->assign("title41", "全国可申请，开卡次月返10元");
+    $smarty->assign("desc41", "1、该卡可以全国申请，不限安徽。");
+    $smarty->assign("desc42", "2、月租19元，可畅享6G全国流量。");
+    $smarty->assign("desc43", "3、前2个月免费使用。");
+    $smarty->assign("id4","liuliang19");
+
+    $smarty->assign("link5", "http://ah.189.cn/wap/combo/CATEGORY_combo4G_49gnbxl.htm?DevCode=AH00F90005/Y34000099437/001");
+    $smarty->assign("img5", "./img/5.jpg");
+    $smarty->assign("title5", "全国流量不限量仅需49元/月");
+    $smarty->assign("title51", "全国可申请，开卡次月返30元");
+    $smarty->assign("desc51", "1、该卡可以全国申请，不限安徽。");
+    $smarty->assign("desc52", "2、月租49元，国内流量不限量使用。");
+    $smarty->assign("desc53", "3、开卡次月返30元，以话费形式冲到开卡号码账户中。");
+    $smarty->assign("id5","liuliang49");
+
+    $smarty->assign("link6", "http://game.fire2333.com/home/ac?action=/home/game/a/1809/g/200002/pt/8397");
+    $smarty->assign("img6", "./img/6.jpg");
+    $smarty->assign("title6", "抢夺王城霸主");
+    $smarty->assign("title61", "热血开战！");
+    $smarty->assign("id6","youxi");
+
+    $smarty->assign("link7", "http://m.ycsbar.com/link/52037691");
+    $smarty->assign("img7", "./img/7.jpg");
+    $smarty->assign("title7", "闪婚闪离的95后女孩");
+    $smarty->assign("title71", "我结婚是为了合法约P!");
+    $smarty->assign("id7","taohuaxiaocunyi");
+
+    $smarty->assign("link8", "http://m.ycsbar.com/link/71423608");
+    $smarty->assign("img8", "./img/8.jpg");
+    $smarty->assign("title8", "一夜成欢 ");
+    $smarty->assign("title81", "一气之下我跟小白脸滚了床单··· ");
+    $smarty->assign("id8","yiyechenghuan");
+
+    $smarty->assign("link9", "https://c.pingan.com/apply/mobile/apply/index.html?scc=950000773&ccp=1a2a3a4a5a8a9a10a11a12a13&showt=0");
+    $smarty->assign("img9", "./img/9.jpg");
+    $smarty->assign("title9", "超高额度快速发卡");
+    $smarty->assign("title91", "发卡成功奖励30元话费");
+    $smarty->assign("id9","pinganxinyongka");
+
+    $smarty->assign("link10", "http://dwz.cn/7DQDl3");
+    $smarty->assign("img10", "./img/10.jpg");
+    $smarty->assign("title10", "超高额度快速发卡");
+    $smarty->assign("title101", "发卡成功奖励20元话费");
+    $smarty->assign("id10","minshengxinyongka");
+
     $smarty->display("ad.html");
 }
 
